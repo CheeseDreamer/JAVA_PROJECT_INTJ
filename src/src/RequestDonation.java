92bc0b72f83040b8bf7bd2a45b1d7472864acb2f
CheeseDreamer <marioszagklas@gmail.com>
2021-04-30 15:06:32 +0300

    RequestDonation done

--- src/src/RequestDonation.java
+++ src/src/RequestDonation.java
@@ -1,2 +1,32 @@
-public class RequestDonation {
+import java.util.Comparator;
+
+public class RequestDonation implements Comparator {
+    //use interface Comparator, or check id for grouping same kind of entities.
+    private Entity entity;
+    private double quantity;
+    public RequestDonation(Entity entity, double quantity)
+    {
+        this.entity = entity;
+        this.quantity = quantity;
+    }
+
+    public Entity getEntity(){
+        return entity;
+    }
+    public double getQuantity(){
+        return quantity;
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        // if they are equal it returns 1, otherwise it returns 0.
+
+        //The excercise asks for RequestDonation tata type, but the method compare needs object data type to be overriden,
+        // will need to look over again when main is done and we can test it
+        if (o1 == o2){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
 }
