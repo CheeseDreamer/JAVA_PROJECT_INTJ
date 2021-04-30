573c27d7c12518fd6cfb38ecae39158ab894143d
CheeseDreamer <marioszagklas@gmail.com>
2021-04-30 13:56:46 +0300

    Material done

--- src/src/Material.java
+++ src/src/Material.java
@@ -1,2 +1,15 @@
-public class Material {
+public class Material extends Entity
+{
+    private double level1,level2,level3;
+    //Beneficiary takes the levels
+
+    public Material(String name, String description, int id)
+    {
+        super(name, description, id);
+    }
+    @Override
+    public String getDetails(){return  super.getDetails() +
+            "\nLevels:" + "\n\tLevel 1: " + level1 + "\n\tLevel 2: " +
+            level2 + "\n\tLevel 3: " + level3 +
+            "\nDonation type: Material";}
 }
