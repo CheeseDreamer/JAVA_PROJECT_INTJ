2c563bfcf5d05bb09984a6dddaafda86fcae2ee8
CheeseDreamer <marioszagklas@gmail.com>
2021-04-30 13:34:01 +0300

    Entity done

--- src/src/Entity.java
+++ src/src/Entity.java
@@ -1,2 +1,15 @@
-public class Entity {
+public abstract class Entity
+{
+    private String name, description;
+    private int id;
+    public Entity(String name, String description, int id)
+    {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+    }
+    public String getEntityInfo(){return name;}
+    public String getDetails(){return  description;}
+    @Override
+    public String toString(){return "Name: " + getEntityInfo() + "\nDetails:" + getDetails(); }
 }
