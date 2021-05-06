65173071b025a3067a6969f4865f9c82b9306d78
CheeseDreamer <marioszagklas@gmail.com>
2021-05-06 15:13:35 +0300

    Fixed the compare in RequestDonation again

--- src/src/RequestDonation.java
+++ src/src/RequestDonation.java
@@ -18,12 +18,12 @@ public class RequestDonation implements Comparator {
     }
 
     @Override
-    public int compare(Entity o1, Entity o2) {
+    public int compare(Object o1, Object o2) {
         // if they are equal it returns 1, otherwise it returns 0.
 
         //The excercise asks for RequestDonation tata type, but the method compare needs object data type to be overriden,
         // will need to look over again when main is done and we can test it
-        if (o1.getId() == o2.getId()){
+        if (o1.equals(o2)){
             return 1;
         }else{
             return 0;
