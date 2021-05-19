014f0137a2c5dd0385a889620853f86f8c1c001e
CheeseDreamer <marioszagklas@gmail.com>
2021-05-20 02:14:00 +0300

    Completed Offers Class

---
+++ src/src/Offers.java
@@ -0,0 +1,12 @@
+public class Offers extends RequestDonationList
+{
+    public Offers()
+    {
+
+    }
+    public void commit(Organization currentDonations, RequestDonationList rdEntities){
+        currentDonations.addCurrentDonations(rdEntities);
+        //if successful, prob done with search function
+        rdEntities.reset();
+    }
+}
