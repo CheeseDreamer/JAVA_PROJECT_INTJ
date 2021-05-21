ff112104735254957321a0af961e658d4f8f39b6
CheeseDreamer <marioszagklas@gmail.com>
2021-05-21 17:14:16 +0300

    Added addOffer() method in Donator

--- src/src/Donator.java
+++ src/src/Donator.java
@@ -1,13 +1,48 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Donator extends User
 {
-    private Offers offersList;
-    public Donator(String name, String phone, Offers offersList)
+    private ArrayList<Offers> offersList;
+    public Donator(String name, String phone)
     {
         super(name,phone);
-        this.offersList = offersList;
     }
+    //ADD METHOD VARIABLES
+
+    public void addOffer(){
+        Scanner s = new Scanner(System.in);
+        int choice;
+        String cancel="n";
+        System.out.println("Choose 1 for Material["+"]"+
+                ", 2 for Service["+"] or other to cancel");
+        choice = s.nextInt();
+        do{
+            String donateChoice;
+            switch (choice) {
+                case 1:
+                    System.out.println("You choose Materials:\n\tDo you want to donate?(y/n)");
+                    donateChoice = s.nextLine();
+                    if (donateChoice =="y"){
+                        offersList.add(new Offers());
+                    }else{ cancel = "y";}
+                    break;
+                case 2:
+                    System.out.println("You choose Services:\n\tDo you want to donate?(y/n)");
+                    donateChoice = s.nextLine();
+                    if (donateChoice =="y"){
+                        offersList.add(new Offers());
+                    }else{ cancel = "y";}
+                    break;
+                default:
+                    System.out.println("Do you want to cancel?(y/n)");
+                    cancel = s.nextLine();
+                    //We can replace cancel var with the Back method, not sure how to make the method for now
+                    break;
+            }
+        }while(cancel!="y");
+    }
+    public void remove() { }
     public void commit(Organization currentDonations, RequestDonationList rdEntities){
         currentDonations.addCurrentDonations(rdEntities);
         //if successful
