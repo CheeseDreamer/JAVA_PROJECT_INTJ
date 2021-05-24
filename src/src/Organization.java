ec5835c506111a2937749e1f7439637f81d0c64a
CheeseDreamer <marioszagklas@gmail.com>
2021-05-25 02:47:22 +0300

    Organization now initializes names and phones

--- src/src/Organization.java
+++ src/src/Organization.java
@@ -4,18 +4,59 @@ public class Organization
 {
     String name;
     Admin admin;
-    ArrayList<Entity> entityList = new ArrayList<Entity>();
-    ArrayList<Donator> donatorList = new ArrayList<Donator>();
-    ArrayList<Beneficiary> beneficiaryList = new ArrayList<Beneficiary>();
-    ArrayList<RequestDonationList> currentDonations = new ArrayList<RequestDonationList>();
+    private int cols=2;
+    ArrayList<Entity> entityList = new ArrayList<>();
+    ArrayList<Donator> donatorList = new ArrayList<>();
+    ArrayList<Beneficiary> beneficiaryList = new ArrayList<>();
+    ArrayList<RequestDonationList> currentDonations = new ArrayList<>();
+    ArrayList<String> adminListName = new ArrayList<>();
+    ArrayList<String> adminListPhone = new ArrayList<>();
+
+    public Organization(){
+        //Admin Names
+        adminListName.add("Marios");
+        adminListName.add("Vicky");
+        adminListName.add("Rainbow");
+        //Admin Phones
+        adminListPhone.add("6922546678");
+        adminListPhone.add("6972643844");
+        adminListPhone.add("6976432144");
+
+        //BeneficiaryList initialization
+        beneficiaryList.add(new Beneficiary());
+        beneficiaryList.add(new Beneficiary());
+        beneficiaryList.add(new Beneficiary());
+        //Beneficiary Names
+        beneficiaryList.get(0).setName("benef1");
+        beneficiaryList.get(1).setName("benef2");
+        beneficiaryList.get(2).setName("benef3");
+        //Beneficiary Phones
+        beneficiaryList.get(2).setPhone("6915720947");
+        beneficiaryList.get(2).setPhone("6983746329");
+        beneficiaryList.get(2).setPhone("6984736150");
+        //DonatorList initialization
+        donatorList.add(new Donator());
+        donatorList.add(new Donator());
+        donatorList.add(new Donator());
+        //Donator Names
+        donatorList.get(0).setName("don1");
+        donatorList.get(1).setName("don2");
+        donatorList.get(2).setName("don3");
+        //Donator Phones
+        donatorList.get(0).setPhone("6934235609");
+        donatorList.get(0).setPhone("6935239504");
+        donatorList.get(0).setPhone("6983205728");
+    }
 
     public void setName(String name){this.name=name;}
     public void setAdmin(){this.admin = admin;}
     public Admin getAdmin(){return this.admin;}
+
     public void setEntityList(ArrayList<Entity> entityList){this.entityList=entityList;}
     public void setDonatorList(ArrayList<Donator> donatorList){this.donatorList=donatorList;}
     public void setBeneficiaryList(ArrayList<Beneficiary> beneficiaryList){this.beneficiaryList=beneficiaryList;}
     public void setCurrentDonations(ArrayList<RequestDonationList> currentDonations){this.currentDonations=currentDonations;}
+
     public void addEntity(Entity entity){
         entityList.add(entity);
         //Needs Exceptions if entity already exists
@@ -39,6 +80,7 @@ public class Organization
     }
     public void removeBeneficiary(Beneficiary benef){
         beneficiaryList.remove(benef);
+        //Exception needed
     }
     public void listEntities(){
         System.out.println("Material/Services");
@@ -49,13 +91,13 @@ public class Organization
     public void listBeneficiaries(){
         System.out.println("Beneficiaries Names:");
         for(var ben: beneficiaryList){
-            System.out.println("\t"+ben.getName());
+            System.out.println("\t"+ben.getName() + " " + ben.getPhone());
         }
     }
     public void listDonators(){
         System.out.println("Donators Names:");
         for(var don:donatorList){
-            System.out.println("\t"+don.getName());
+            System.out.println("\t"+don.getName() + " " + don.getPhone());
         }
     }
     //currentDonations setters, getters are different from wrappers, also might need to make them
