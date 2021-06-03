4cdcff6816b6b487267170b4f924bf64aa275003
CheeseDreamer <marioszagklas@gmail.com>
2021-06-03 10:42:22 +0300

    Unfinished added modify, add and commit methods validRequestDonation is set to return boolean commit and valid are not finished

--- src/src/Requests.java
+++ src/src/Requests.java
@@ -1,38 +1,80 @@
+import java.util.Scanner;
+
 public class Requests extends RequestDonationList{
 
     @Override
-    public void add(RequestDonation obj, Organization org) {
-        for(int i=0;i<getRdEntities().size();i++) {
-            if (getRdEntities().get(i).contains(obj)) {
-                // enhmerwsh posothtas tou obj
-                obj.addQuantity(getRdEntities().get(0).get(i).getQuantity());
-            } else if (!(getRdEntities().get(i).contains(obj))) {
-                if(obj.getEntityType().equals("Material")) {
-                    getRdEntities().get(0).add(obj);
-                }else if(obj.getEntityType().equals("Service")){
-                    getRdEntities().get(1).add(obj);
+    public void add(RequestDonation rd, Organization org) {
+        boolean found = false;
+        if (org.getEntityList().get(0).contains(rd.getEntity()) || org.getEntityList().get(1).contains(rd.getEntity())) {
+            //if(validRequestDonation()){}else{//throw exception}
+            for (int i = 0; i < getRdEntities().size(); i++) {
+                for (int j = 0; j < getRdEntities().get(i).size(); j++) {
+                    if (RequestDonation.compare(rd, getRdEntities().get(i).get(j))) {
+                        // enhmerwsh posothtas tou obj
+                        getRdEntities().get(i).get(j).addQuantity(rd.getQuantity());
+                        System.out.println("Added Quantity");
+                        found = true;
+                        break;
+                    } else if (!(getRdEntities().get(i).contains(rd))) {
+                        if (rd.getEntityType().equals("Material")) {
+                            getRdEntities().get(0).add(rd);
+                        } else if (rd.getEntityType().equals("Service")) {
+                            getRdEntities().get(1).add(rd);
+                        }
+                    }
                 }
             }
+            if(!found) {//if you are requesting it for the first time
+                System.out.println("Requesting entity for the first time");
+                if (rd.getEntityType().equals("Material")) {
+                    getRdEntities().get(0).add(new RequestDonation(rd.getEntity(), rd.getQuantity()));
+                    System.out.println("Added Material");
+                } else if (rd.getEntityType().equals("Service")) {
+                    getRdEntities().get(1).add(new RequestDonation(rd.getEntity(), rd.getQuantity()));
+                    System.out.println("Added Service");
+                }
+            }
+        }else{
+            System.out.println("Requested Entity not found in Organization!");
+            //Exception
         }
     }
 
-    @Override
-    public void modify(RequestDonation obj, int index) {    //process of quantity
-        //index: 1 for subtraction, 2 for addition
-        if (index == 2) {
-            //obj.addQuantity();
-        } else if (index == 1) {
-            //obj.subQuantity();
-        }else{//(index!=1||index!=2)
-            //Throws Exception
-        }
-        if (obj.getQuantity() < 0 && index == 1) {       //can't reduce the quantity, already 0.
-            //Throws Exception
+    //@Override
+    public void modify(int modifyChoice, int offerID, Organization org, Scanner scan) {    //process of quantity
+        int modQuantity;
+        switch (modifyChoice) {
+            case 1:
+                System.out.print("Add Quantity: ");
+                modQuantity = scan.nextInt();
+                org.getBeneficiaryList().get(Beneficiary.getPos()).getRequestsList().getWithID(offerID).addQuantity(modQuantity);
+                System.out.println("Added "+ modQuantity +" quantity");
+                System.out.println("[id]: "+ offerID +" [Current Quantity]: " + org.getBeneficiaryList().get(Beneficiary.getPos()).getRequestsList().getWithID(offerID).getQuantity());
+                break;
+            case 2:
+                System.out.print("Sub Quantity: ");
+                modQuantity = scan.nextInt();
+                if(org.getBeneficiaryList().get(Beneficiary.getPos()).getRequestsList().getWithID(offerID).getQuantity()-modQuantity>=0) {
+                    org.getBeneficiaryList().get(Beneficiary.getPos()).getRequestsList().getWithID(offerID).subQuantity(modQuantity);
+
+                }else{
+                    org.getBeneficiaryList().get(Beneficiary.getPos()).getRequestsList().getWithID(offerID).subQuantity(org.getBeneficiaryList().get(Beneficiary.getPos()).getRequestsList().getWithID(offerID).getQuantity());
+                    System.out.println("[Request Donation Quantity is set to 0]");
+                }
+                System.out.println("Subtracted "+ modQuantity +" quantity");
+                System.out.println("[id]: "+ offerID +" [Current Quantity]: " + org.getBeneficiaryList().get(Beneficiary.getPos()).getRequestsList().getWithID(offerID).getQuantity());
+                break;
+            case 3://Back
+                break;
+            default:
+                break;
         }
     }
 
-    public void validRequestDonation(){ }
-    public void commit(){ }
+    public boolean validRequestDonation(){
+        return false;
+    }
+    public void commit(Organization org){ }
     public void addOffer(){
         System.out.println("Choose 1 for Material, 2 for Services:");
     }
